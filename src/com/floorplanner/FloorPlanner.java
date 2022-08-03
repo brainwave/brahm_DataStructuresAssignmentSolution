2d5dca13392fbd16eae42db1ee0259dd5351ade2
Brahm Prakash Mishra <bpmishra92@gmail.com>
2022-08-03 22:37:13 +0530

    Solved question 1 in better way using max heaps

--- src/com/floorplanner/FloorPlanner.java
+++ src/com/floorplanner/FloorPlanner.java
@@ -3,7 +3,6 @@ package com.floorplanner;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class FloorPlanner {
 	/*
@@ -51,41 +50,35 @@ public class FloorPlanner {
 		Integer[] floorSizesSorted = this.floorSizes.clone();
 		Arrays.sort(floorSizesSorted, Collections.reverseOrder());
 
-		Stack<Integer> assembledFloorStack = new Stack<Integer>(); // use stack for storing integers parsed from the
-																	// input array
+		MaxHeap maxheap = new MaxHeap(this.numFloors); // create max heap to store the floors
 
 		int j = 0; // index to keep track of position in sorted array
 
 		for (int i = 0; i < this.numFloors; ++i) { // loop from 0 till numFloors, i to keep track of the index
 
-			if (this.floorSizes[i] == floorSizesSorted[j]) { // whenever the index matches the largest element
-																// (currently at j)
+			System.out.println("Day: " + (i + 1));
 
-				j = (i == this.numFloors - 1) ? i : (i + 1); //
+			if (this.floorSizes[i] == floorSizesSorted[j]) { // Check if the current element in unsorted stack equals
+																// the largest element in the sorted stack
 
-				System.out.println("Day: " + (i + 1));
+				System.out.printf(floorSizes[i] + " "); // print current floor number
+				j++; // go to next largest item in the sorted list
 
-				if (assembledFloorStack != null) {
-					System.out.print(this.floorSizes[i] + " ");
-					while (!assembledFloorStack.isEmpty()) {
-						int val = assembledFloorStack.pop(); // last element in the stack.
-						if (val >= floorSizesSorted[j]) { // if the last element in the stack is > next consecutive
-															// element in the sorted array
-							System.out.print(val + " "); // print it
-						} else {
-							assembledFloorStack.push(val);
-							j -= assembledFloorStack.size();
-							break;
-						}
+				while (maxheap.getSize() > 0) {
+					if (maxheap.viewMax() < floorSizesSorted[j]) // if the maximum of current heap is lower than the
+																	// next max element in sorted array, do nothing
+						break;
+					else {
+						System.out.printf(maxheap.extractMax() + " "); // the next element in stack is same as the next
+																		// //
+																		// largest array in sorted array
+						j = (j < numFloors - 1) ? (j + 1) : (j); // increment max heap
 					}
-					System.out.println("");
-				} else {
-					System.out.println(this.floorSizes[i]);
 				}
-			} else {
-				System.out.println("Day: " + (i + 1));
-				assembledFloorStack.push(this.floorSizes[i]);
-			}
+
+				System.out.println();
+			} else
+				maxheap.insert(this.floorSizes[i]);
 		}
 	}
 }
--- src/com/floorplanner/Main.java
+++ src/com/floorplanner/Main.java
@@ -14,7 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		FloorPlanner fp = new FloorPlanner();
 		fp.getFloorSizes();
-		// fp.printConstructionOrder();
+		fp.printConstructionOrder();
 	}
 
 }
